ede7529bd27c2cb8a22b31e230c91d3558992aec
Stephan Gabler <stephan.gabler@gmail.com>
2010-06-29 08:58:48 +0200

    cleaned up version of the old code, removed reduncancies

--- Matrixinvertierung.m
+++ Matrixinvertierung.m
@@ -1,14 +1,14 @@
 inversible=zeros(10,10,12);
 for gert=7:12
     hilfsgert=cell2mat(probanden_10_2(gert));
-for z=1:12
-for i=1:10
-for k=1:10
-     inversible(i,11-k,z)= hilfsgert(i,k,z);
+    for z=1:12
+        for i=1:10
+            for k=1:10
+                inversible(i,11-k,z)= hilfsgert(i,k,z);
+            end
+        end
+        probanden_10_2{gert}=inversible;
     end
-end
-    probanden_10_2{gert}=inversible;
-end
 end
 
 
